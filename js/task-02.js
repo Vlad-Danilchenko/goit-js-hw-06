dd75b851877ecd62dcd493dbb1f94c2505795a60
Freasy-Vlad <vladdanilchenkodune@gmail.com>
2022-09-19 23:17:31 +0300

    add task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const elements = ingredients.map((ingredient) => {
+  const ingredientItemEl = document.createElement("li");
+  ingredientItemEl.textContent = ingredient;
+  ingredientItemEl.classList.add("item");
+  return ingredientItemEl;
+});
+
+console.log(elements);
+
+const ingredientsUlEl = document.querySelector("#ingredients");
+ingredientsUlEl.append(...elements);
