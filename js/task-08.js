1392c9524687c6333e1a9f25d61aba7b660084fa
Freasy-Vlad <vladdanilchenkodune@gmail.com>
2022-09-28 01:07:00 +0300

    add task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formRef = document.querySelector(".login-form");
 
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.target;
+  // console.log(email.value);
+  // console.log(password.value);
+
+  if (email.value === "" || password.value === "") {
+    alert("Заповніть всі поля");
+    return;
+  }
+
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(userData);
+  return formRef.reset();
+};
+
+formRef.addEventListener("submit", handleSubmit);
